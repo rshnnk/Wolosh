89703a708b184e40079aa75069d43b6bdec3e242
ruslanarshinnik <ruslanarshinnik@gmail.com>
2015-03-01 21:19:15 +0200

    AddingMainFiles

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    $("#owl-example").owlCarousel({
+        items : 1,
+        itemsDesktop : [1199,1],
+        itemsDesktopSmall : [980,1],
+        itemsTablet: [768,1],
+        autoPlay : true,
+        stopOnHover : true
+    });
+
+
+});
